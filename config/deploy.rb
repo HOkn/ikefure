e992c8ea816b6c8c9a61f86b864a795518de1737
hirooooooo <hiroaki.suzuki@codecamp.jp>
2015-05-09 15:57:04 +0900

    capistrano設定追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,21 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :sidekiq_role, :web
+
+set :application, 'ikefure'
+set :branch, 'master'
+set :deploy_to, '/var/www/html'
+set :scm, :git
+set :use_sudo, false
+set :log_level, :debug
+#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets public/system public/assets}
+set :keep_releases, 3
+set :default_env, { path: "/usr/local/bin:$PATH" }
+set :default_shell, '/bin/bash -l'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
@@ -23,26 +33,12 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+#set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
