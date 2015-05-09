1c2da7aef2a4326aab5321730b868b0fa5f66a74
Haru Okaniwa <haruokny@gmail.com>
2015-05-09 16:48:08 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'faces/admin' => 'faces#admin'
-  resources :faces
+  resources :faces do
+    collection do
+      get :admin
+    end
+  end
   resources :raws
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
